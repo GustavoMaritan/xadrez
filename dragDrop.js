a8fa405616dba71a0d1dbadca02e4b2ecaee86a6
Gustavo <gustavomaritan@gmail.com>
2019-01-08 23:56:44 -0200

    asd

--- dragDrop.js
+++ dragDrop.js
@@ -24,12 +24,15 @@ function drop(ev) {
         return ev.preventDefault();
 
     let pc = document.getElementById(data);
-
     let old = $(pc).closest('div');
-
     $(pc).attr('data-pos', pos.join('-'));
-
     $(ev.target).html(pc);
+    let classe = $(pc).attr('data-cor') == 'clara' ? 'brancas' : 'negras';
+
+    let total = $(`div.${classe}>.jogadas>p`).length;
+
+    $(`div.${classe}>.jogadas`).prepend(`<p>${total + 1} - ${$(pc).attr('data-tipo')} - ${$(ev.target).attr('data-pos')}</p>`)
+
     //resetMovs();
     efetivoMov(ev.target, old);
 }
