382fe1115fbff65004dd3ca6db2ca1ff5164b3cc
Gustavo <gustavomaritan@gmail.com>
2019-01-13 09:58:28 -0200

    v2 - a

--- js/create.js
+++ js/create.js
@@ -60,12 +60,13 @@ window.$create = (function () {
 
     function _init() {
         _tabuleiro();
-        if (false) {
+        if (true) {
             // GAMBI SÓ PRA TESTES
             $('.casa[data-pos="1-1"]').html($create.html('clara', new Date().getTime(), 'torre', "1-1", 'media'));
             $('.casa[data-pos="1-4"]').html($create.html('clara', new Date().getTime(), 'rei', "1-4", 'grande'));
             $('.casa[data-pos="1-8"]').html($create.html('clara', new Date().getTime(), 'torre', "1-8", 'media'));
             $('.casa[data-pos="8-5"]').html($create.html('escura', new Date().getTime(), 'rei', "8-5", 'grande'));
+            $('.casa[data-pos="8-8"]').html($create.html('escura', new Date().getTime(), 'torre', "8-8", 'media'));
             return
         };
         _new(1);
